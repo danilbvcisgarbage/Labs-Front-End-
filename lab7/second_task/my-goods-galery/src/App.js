38ee8a1553242c3125083106ad0fd43659680db9
danilbvcisgarbage <121589016+danilbvcisgarbage@users.noreply.github.com>
2024-12-07 17:32:33 +0100

    Add files via upload

---
+++ lab7/second_task/my-goods-galery/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GoodsGallery from './components/GoodsGallery';
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center' }}>Goods Gallery</h1>
+      <GoodsGallery />
+    </div>
+  );
+}
+
+export default App;
